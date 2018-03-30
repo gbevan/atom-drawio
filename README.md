956d9ca0e323b9f2967bedf141cc862697da5355
Graham Bevan <graham.bevan@ntlworld.com>
2018-03-30 19:31:59 +0100

    project start

---
+++ README.md
@@ -0,0 +1,4 @@
+# Package to provide embedded Draw.io diagram editting support in Atom
+
+This is an Alpha release - please feel free to contribute and improve this
+package.
---
+++ spec/atom-drawio-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomDrawioView from '../lib/atom-drawio-view';
+
+describe('AtomDrawioView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
